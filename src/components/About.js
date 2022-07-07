04c691ae4429b0cf8b008e63c0f1da10b7a59e5c
KasparasBal <kasparasbalys@gmail.com>
2022-07-07 12:25:36 +0300

    Commit 5: small About page added

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,18 @@
+import "../styles/About.css";
+
+const beans_back =
+  "https://images.unsplash.com/photo-1580933073521-dc49ac0d4e6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80";
+
 const About = () => {
-  return <></>;
+  return (
+    <div className="about_container">
+      <div style={{ backgroundImage: `url(${beans_back})` }} className="beans">
+        <p className="about_description">
+          Finest Coffee Around, Different Blends, Beans and Ground Coffee.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
