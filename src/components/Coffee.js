0c54456a90a4aabe6154950ebeb1a751504718d7
KasparasBal <kasparasbalys@gmail.com>
2022-07-07 12:15:44 +0300

    Commit 4: Added Products.

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,5 +1,39 @@
+import Product from "./Product";
+import { useEffect, useState } from "react";
+
+import "../styles/Coffee.css";
+
 const Coffee = () => {
-  return <>Coffee</>;
+  const [coffee, setCoffee] = useState(null);
+
+  useEffect(() => {
+    const fetchCoffee = async () => {
+      const response = await fetch("http://localhost:4000/api/coffee"); // Normally with corse we leave in http://localhost:3001
+      const json = await response.json(); // array of book objects
+
+      if (response.ok) {
+        setCoffee(json);
+      }
+    };
+
+    fetchCoffee();
+  }, []);
+
+  return (
+    <div className="coffee_page_container">
+      <div className="coffee_page">
+        {coffee &&
+          coffee.map((coffee) => (
+            <Product
+              title={coffee.title}
+              year={coffee.type}
+              price={coffee.description}
+              image={coffee.image}
+            />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Coffee;
